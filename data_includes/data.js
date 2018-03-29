42d330dd9330e1bb27372d91a0a33c984e76c380
Teunisse <j.j.teunisse@uu.nl>
2018-03-29 16:24:09 +0200

    Test rshuffle

--- data_includes/data.js
+++ data_includes/data.js
@@ -1,4 +1,4 @@
-var shuffleSequence = seq("intro", "instructions", "item1");
+var shuffleSequence = seq("intro", "instructions", rshuffle(endsWith("_1"), endsWith("_2")));
 
 var completionMessage = "Uw antwoorden zijn verzonden naar de server. Bedankt voor uw deelname!"
 
@@ -17,8 +17,20 @@ var items =
 [
   [ "intro", "Form", { continueMessage: "Klik hier om door te gaan", html: { include: "intro.html" } } ],
   [ "instructions", "Message", { html: { include: "instructions.html" } } ], 
-  [ "item1",
+  [ "item1_1",
     "Message", { html: [ [ "div", { style: "text-align: center;" } ], [ "p", "Multifex doorboort razendsnel de hemel." ] ] },
+    "Question", { q: "Zou je dit product kopen?" }
+  ]
+  [ "item1_2",
+    "Message", { html: [ [ "div", { style: "text-align: center;" } ], [ "p", "De hemel wordt razendsnel doorboord door Multifex." ] ] },
+    "Question", { q: "Zou je dit product kopen?" }
+  ]
+  [ "item2_1",
+    "Message", { html: [ [ "div", { style: "text-align: center;" } ], [ "p", "<camera> fotografeert onmiddelijk jouw herinnering." ] ] },
+    "Question", { q: "Zou je dit product kopen?" }
+  ]
+  [ "item2_2",
+    "Message", { html: [ [ "div", { style: "text-align: center;" } ], [ "p", "Jouw herinnering wordt onmiddelijk gefotografeerd door <camera>." ] ] },
     "Question", { q: "Zou je dit product kopen?" }
   ]
 ];
