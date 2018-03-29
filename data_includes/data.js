1340ce0159965d0a0157325270de480f0f6a3cc5
Teunisse <j.j.teunisse@uu.nl>
2018-03-29 15:37:17 +0200

    Message/Question

--- data_includes/data.js
+++ data_includes/data.js
@@ -7,12 +7,15 @@ var progressBarText = "Voortgang"
 var defaults =
 [
   "Form", { hideProgressBar: true },
-  "Message", { hideProgressBar: true }
+  "Message", { hideProgressBar: true, continueMessage: "Klik hier om door te gaan" }
 ];
 
 var items =
 [
-  ["intro", "Form", {continueMessage: "Klik hier om door te gaan", html: { include: "intro.html" } } ],
-  ["instructions", "Message", {continueMessage: "Klik hier om door te gaan", html: { include: "instructions.html" } } ], 
-  ["item1", "AcceptabilityJudgment", { s: "Multifex doorboort razendsnel de hemel", q: "Zou je dit product kopen?", as: ["1", "2", "3", "4", "5", "6", "7"], presentAsScale: true } ]
+  [ "intro", "Form", { continueMessage: "Klik hier om door te gaan", html: { include: "intro.html" } } ],
+  [ "instructions", "Message", { html: { include: "instructions.html" } } ], 
+  [ "item1",
+    "Message" { s: {html: "<p>Multifex doorboort razendsnel de hemel.</p>"}},
+    "Question", { q: "Zou je dit product kopen?", as: ["1", "2", "3", "4", "5", "6", "7"], presentAsScale: true }
+  ]
 ];
