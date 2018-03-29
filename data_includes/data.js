cd4cce420cf745ae12f57b27bfd3fb95c4b0e84c
Teunisse <j.j.teunisse@uu.nl>
2018-03-29 15:59:17 +0200

    HTMLfix I

--- data_includes/data.js
+++ data_includes/data.js
@@ -17,7 +17,7 @@ var items =
   [ "intro", "Form", { continueMessage: "Klik hier om door te gaan", html: { include: "intro.html" } } ],
   [ "instructions", "Message", { html: { include: "instructions.html" } } ], 
   [ "item1",
-    "Message", { html: "<p align=&quot;center&quot;>Multifex doorboort razendsnel de hemel.</p>" },
+    "Message", { html: [ "p", { style: "text-align: center" }, "Multifex doorboort razendsnel de hemel." ] },
     "Question", { q: "Zou je dit product kopen?", as: ["1", "2", "3", "4", "5", "6", "7"], presentAsScale: true }
   ]
 ];
