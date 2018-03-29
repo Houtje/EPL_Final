d4b9ce6380819dcc1f22861e0d000ce2238f178e
Teunisse <j.j.teunisse@uu.nl>
2018-03-29 15:32:32 +0200

    AJtest

--- data_includes/data.js
+++ data_includes/data.js
@@ -1,4 +1,4 @@
-var shuffleSequence = seq("intro", "instructions");
+var shuffleSequence = seq("intro", "instructions", "item1");
 
 var completionMessage = "Uw antwoorden zijn verzonden naar de server. Bedankt voor uw deelname!"
 
@@ -14,4 +14,5 @@ var items =
 [
   ["intro", "Form", {continueMessage: "Klik hier om door te gaan", html: { include: "intro.html" } } ],
   ["instructions", "Message", {continueMessage: "Klik hier om door te gaan", html: { include: "instructions.html" } } ], 
+  ["item1", "AcceptabilityJudgment", { s: "Multifex doorboort razendsnel de hemel", q: "Zou je dit product kopen?", as: ["1", "2", "3", "4", "5", "6", "7"], presentAsScale: true } ]
 ];
