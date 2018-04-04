d2394f1c9cd46583702b81bd6c29b2742709b50e
a.n.vannnieuwenhuijsen <a.n.vannieuwenhuijsen@students.uu.nl>
2018-04-04 20:52:24 +0200

    geombar tests

--- r/cleanup2.R
+++ r/cleanup2.R
@@ -1,13 +1,14 @@
 # EPL final project - data processing script
 
-#setwd("C:/Users/Joris/Documents/GitHub/EPL_Final/r")
+#setwd("C:/Users/Joris/Desktop/meuk")
 setwd("C:/Users/Arianne/Documents/EPL_Final/r/")
 
-library(numbers)
+# plot library
 library(ggplot2)
+library(numbers)
 
 tf <- function(q, tform){
-  return(table(cres[(cres$question == q) & (cres$tform == tform),]$answer))
+  return(table(as.numeric(as.character(cres[(cres$question == q) & (cres$tform == tform),]$answer))))
 }
 
 bplot <- function(q, topic){
@@ -19,15 +20,15 @@ bplot <- function(q, topic){
 res <- read.csv("results.csv", header=FALSE, comment.char="#", na.strings="NULL", encoding="UTF-8")
 com <- as.vector(res[(res$V6 == "comments") & (res$V9 != ""),]$V9)
 cres <- res[(res$V4 != 1) & (res$V4 != 2),]
-cres <- data.frame(ID=cres$V1, product=floor(cres$V4/4), tform=mod((cres$V4), 4), question=cres$V5,
-                   answer=as.numeric(as.character(cres$V9)), rt=cres$V11)
-kernels <- cres[cres$tform == 0,]
+cres <- data.frame(ID=cres$V1, tform=mod((cres$V4), 4), question=cres$V5, answer=cres$V9, rt=cres$V11)
 
-mean_prod_1 <- aggregate(cres$answer, by=list(prod=cres$product, q=cres$question), FUN=mean)
-mean_kern_1 <- aggregate(kernels$answer, by=list(prod=kernels$product, q=kernels$question), FUN=mean)
+bplot(1, "Clarity")
+bplot(2, "Interest")
+bplot(3, "Recommendation")
+bplot(4, "Buying")
 
-# bplot(1, "Clarity")
-# bplot(2, "Interest")
-# bplot(3, "Recommendation")
-# bplot(4, "Buying")
-
+ggplot(summary2, aes(x=, y=rt, fill=Placement)) + 
+  geom_bar(position=position_dodge(), stat="identity") +
+  geom_errorbar(aes(ymin=rt-se, ymax=rt+se),
+                width=.2,                    # Width of the error bars
+                position=position_dodge(.9)) + ylab("Reaction Time")
